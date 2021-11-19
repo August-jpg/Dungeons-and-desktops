45e60c5613ef050b0b9f080b50915e1670f25b90
August Kuehl <turtlethedragon@icloud.com>
2021-11-19 08:40:22 -0600

    Update adventure.js

--- dungeonclicker/adventure.js
+++ dungeonclicker/adventure.js
@@ -113,8 +113,68 @@ function generateHeroicQuests() {
 
 function generateQuests() {
 		for (var i = 0; i < 15; i++) {
-			quests.push("You <span class='xp'>conquer your fear of " + getRandom(fears) + "</span>! You earn <span class='coin'>#coin#</span>.");
+			quests.push("You <span class='xp'>slay " + getRandom(races) + " " + getRandom(classes) + "</span>! You earn <span class='coin'>#coin#</span>.");
+			quests.push("You <span class='xp'>slay a " + getRandom(prefixes) + " " + getRandom(pests) + "</span>! You earn <span class='coin'>#coin#</span>.");
+			quests.push("You <span class='xp'>gather some " + getRandom(gatherables) + " for " + getRandom(questgivers) + "</span>! You earn <span class='coin'>#coin#</span>.");
 		}
+		quests.push("You <span class='xp'>help fend off " + getRandom(races) + " incursion</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>stop the century-old war between " + getRandom(gFolks) + " and " + getRandom(bFolks) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help stop a " + getRandom(prefixes) + " " + getRandom(pests) + " invasion</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help catch " + getRandom(races) + " " + getRandom(classes) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>unravel " + getRandom(questgivers) + "'s evil plans</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>unmask a " + getRandom(adventurers) + "'s true intentions</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>thwart the heinous plans of an evil " + getRandom(adventurers) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>expose a treacherous " + getRandom(adventurers) + "'s agreement with " + getRandom(races) + " " + getRandom(spellcasters) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>stop an evil " + getRandom(spellcasters) + "'s ritual</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>hunt down a rogue " + getRandom(spellcasters) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>recover an ancient relic from the hands of an evil " + getRandom(spellcasters) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>scort " + getRandom(questgivers) + " through the wilderness</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>slay a " + getRandom(colors) + " slime</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>slay a " + getRandom(colors) + " jelly</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>paint a " + getRandom(objects) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>fix a " + getRandom(objects) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help rebuild a " + getRandom(objects) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>find a lost " + getRandom(pets) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>rescue a " + getRandom(pets) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>find a little boy's " + getRandom(toys) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>bathe a " + getRandom(prefixes) + " " + getRandom(pets) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help treat a " + getRandom(spellcasters) + "'s " + getRandom(prefixes) + " " + getRandom(pets) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>fish a " + getRandom(prefixes) + " " + getRandom(fish) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>hunt a " + getRandom(prefixes) + " " + getRandom(game) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>catch a " + getRandom(prefixes) + " " + getRandom(pests) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>get some " + getRandom(drinks) + " for a thirsty " + getRandom(adventurers) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>get some " + getRandom(food) + " for a hungry " + getRandom(adventurers) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a housewive cook some " + getRandom(food) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(spellcasters) + " research a new spell</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help celebrate a " + getRandom(adventurers) + "'s birthday</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " get a girlfriend</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " find inner peace</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>find " + getRandom(questgivers) + "'s lost sack of " + getRandom(gatherables) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>solve " + getRandom(questgivers) + "'s " + getRandom(pests) + " problem</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " reforge an ancient artifact</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " kill a dire " + getRandom(pests) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " complete a quest</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " find " + getRandom(posesives) + " lost " + getRandom(relatives) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a young " + getRandom(adventurers) + " confront " + getRandom(posesives) + " father</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a shy " + getRandom(adventurers) + " overcome " + getRandom(posesives) + " awkwardness</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " avenge " + getRandom(posesives) + " " + getRandom(relatives) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a " + getRandom(adventurers) + " rescue " + getRandom(posesives) + " party</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>help a novice " + getRandom(adventurers) + " pass " + getRandom(posesives) + " test</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>rescue a " + getRandom(spellcasters) + "'s " + getRandom(pests) + " familiar</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>end a dispute between a " + getRandom(adventurers) + " and " + getRandom(races) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>clear an innocent " + getRandom(adventurers) + "'s name</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>redeem an old " + getRandom(adventurers) + " before " + getRandom(posesives) + " death</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>stop an assassination plot against an important " + getRandom(adventurers) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>deliver a " + getRandom(deliverables) + " for " + getRandom(questgivers) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>look inside " + getRandom(containers) + "</span>! You find <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>solve the riddle of an eccentric " + getRandom(spellcasters) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>conquer your fear of " + getRandom(fears) + " a " + getRandom(prefixes) + " " + getRandom(pests) + "</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>win " + getRandom(contests) + " contest</span>! The prize is <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>play dice with " + getRandom(races) + "</span>! You win <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>play cards with " + getRandom(races) + "</span>! You win <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>find the cure for a terrible plague</span>! You earn <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>break a clay pot</span>! You find <span class='coin'>#coin#</span>.");
+		quests.push("You <span class='xp'>slice a bush</span>! You find <span class='coin'>#coin#</span>.");
 }
 
 function getRandom(arr) {
